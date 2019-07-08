0cea1c70cdf8cbb011733c59cb9e6c3e94170c2f
Quantumapple <jongho.lee@cern.ch>
2019-07-07 23:32:00 -0500

    change quantize code

--- condor/generate_mc_pythia.sh
+++ condor/generate_mc_pythia.sh
@@ -8,11 +8,11 @@ cd ${delphesDIR}
 mkdir -p Results/job_$1
 ./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my200PU.tcl examples/Pythia8/generatePileUp.cmnd Results/job_$1/SingleEle200PU.root
 
-cp Example9.C Example9_$1.C
-sed -i "718s/results.root/results_$1.root/g" Example9_$1.C
+cp Example10.C Example10_$1.C
+sed -i "718s/results.root/results_$1.root/g" Example10_$1.C
 
 root -l -b << EOF
-.L Example9_$1.C
+.L Example10_$1.C
 Example9("Results/job_$1/SingleEle200PU.root")
 .q
 EOF
