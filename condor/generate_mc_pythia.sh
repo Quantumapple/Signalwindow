c0cd2a0e4f6cf10ce2f728769bc8aca952446fcc
Quantumapple <jongho.lee@cern.ch>
2019-07-14 21:34:57 -0500

    condor for Example15

--- condor/generate_mc_pythia.sh
+++ condor/generate_mc_pythia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-delphesDIR=/cms/ldap_home/jongho/L1PixelTrigger/Delphes/Delphes-3.4.1 
+delphesDIR=/cms/scratch/jongho/Delphes/Delphes-3.4.1
 
 source ${delphesDIR}/setenv.sh  
 cd ${delphesDIR}
@@ -8,17 +8,17 @@ cd ${delphesDIR}
 tar -axvf submit.tar 
 
 mkdir -p Results/job_$1
-./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my.tcl examples/Pythia8/configParticleGun.cmnd Results/job_$1/SingleEle200PU.root ## Single Ele NOPU
+#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my.tcl examples/Pythia8/configParticleGun.cmnd Results/job_$1/SingleEle200PU.root ## Single Ele NOPU
 #./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my200PU.tcl examples/Pythia8/configParticleGun.cmnd Results/job_$1/SingleEle200PU.root ## Single Ele 200PU
-#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my200PU.tcl examples/Pythia8/generatePileUp.cmnd Results/job_$1/SingleEle200PU.root ## Minbias 200PU
+./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my200PU.tcl examples/Pythia8/configNoLHE.cmnd Results/job_$1/SingleEle200PU.root ## Minbias 200PU
 
-cp Example11.C Example11_$1.C
+cp Example15.C Example15_$1.C
 tempNum=$1
-sed -i "655s/results.root/results_${tempNum}.root/g" Example11_$1.C
+sed -i "635s/results.root/results_${tempNum}.root/g" Example15_${tempNum}.C
 
 root -l -b << EOF
-.L Example11_$1.C
-Example11("Results/job_$1/SingleEle200PU.root")
+.L Example15_${tempNum}.C
+Example15("Results/job_${tempNum}/SingleEle200PU.root")
 .q
 EOF
 
@@ -30,9 +30,10 @@ cd -
 cp x509up_u* /tmp
 voms-proxy-info -all
 
-#xrdcp txtfile root://cluster142.knu.ac.kr//store/user/jongho/MinBias200PU_Delphes/Test1
-mv results_${tempNum}.root Results/job_$1/results_${tempNum}.root
-rm Results/job_$1/SingleEle200PU.root
-rm Example11_$1.C
-#rm results_${tempNum}.root
+xrdcp results_${tempNum}.root root://cluster142.knu.ac.kr//store/user/jongho/MinBias200PU_Delphes/0000
+#xrdcp results_${tempNum}.root root://c/store/user/jongho/MinBias200PU_Delphes/Test1
+#mv results_${tempNum}.root Results/job_${tempNum}/results_${tempNum}.root
+rm Results/job_${tempNum}/SingleEle200PU.root
+rm Example15_${tempNum}.C
+rm results_${tempNum}.root
 
