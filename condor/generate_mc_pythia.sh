3a97fd924e3e54f514f9f79b4eda354a4a5854ff
Quantumapple <jongho.lee@cern.ch>
2019-07-28 22:00:55 -0500

    job submission

--- condor/generate_mc_pythia.sh
+++ condor/generate_mc_pythia.sh
@@ -9,18 +9,19 @@ cd ${delphesDIR}
 
 tempNum=$1
 mkdir -p Results/job_${tempNum}
-#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_myV3.tcl examples/Pythia8/configParticleGun.cmnd Results/job_$1/SingleEle200PU.root ## Single Ele NOPU
-#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_my200PU.tcl examples/Pythia8/configParticleGun.cmnd Results/job_$1/SingleEle200PU.root ## Single Ele 200PU
-#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_EcalUpdate200PU.tcl examples/Pythia8/configNoLHE.cmnd Results/job_${tempNum}/SingleEle200PU.root ## Minbias 200PU
-./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_190727_200PU.tcl examples/Pythia8/generatePileUpCMS.cmnd Results/job_${tempNum}/SingleEle200PU.root 
-#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_myV3.tcl examples/Pythia8/configLHE.cmnd Results/job_${tempNum}/SingleEle200PU.root ## ZToNuNu 200PU
+./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_Final200PU.tcl examples/Pythia8/configParticleGun.cmnd Results/job_${tempNum}/SingleEle200PU.root 
+#./DelphesPythia8 cards/CMS_PhaseII/CMS_PhaseII_PixelStudies_Final200PU.tcl examples/Pythia8/generatePileUpCMS.cmnd Results/job_${tempNum}/SingleEle200PU.root 
 
-cp Example18.C Example18_${tempNum}.C
-sed -i "587s/results.root/results_${tempNum}.root/g" Example18_${tempNum}.C
+sleep 3
+
+cp Example21.C Example21_${tempNum}.C
+sed -i "698s/results.root/results_${tempNum}.root/g" Example21_${tempNum}.C
+
+sleep 3
 
 root -l -b << EOF
-.L Example18_${tempNum}.C
-Example18("Results/job_${tempNum}/SingleEle200PU.root")
+.L Example21_${tempNum}.C
+Example21("Results/job_${tempNum}/SingleEle200PU.root")
 .q
 EOF
 
@@ -32,11 +33,10 @@ cd -
 #cp x509up_u* /tmp
 #voms-proxy-info -all
 
-#xrdcp results_${tempNum}.root root://cluster142.knu.ac.kr//store/user/jongho/MinBias200PU_Delphes/0000
-xrdcp -f results_${tempNum}.root root://cms-xrdr.private.lo:2094//xrd/store/user/jongho/Minbias/0000
-#xrdcp -f results_${tempNum}.root root://cms-xrdr.private.lo:2094//xrd/store/user/jongho/SingleEle/0000
+#xrdcp -f results_${tempNum}.root root://cms-xrdr.private.lo:2094//xrd/store/user/jongho/Minbias/0000
+xrdcp -f results_${tempNum}.root root://cms-xrdr.private.lo:2094//xrd/store/user/jongho/SingleEle200PU/0000
 #mv results_${tempNum}.root Results/job_${tempNum}/results_${tempNum}.root
 rm Results/job_${tempNum}/SingleEle200PU.root
-rm Example18_${tempNum}.C
+rm Example21_${tempNum}.C
 rm results_${tempNum}.root
 
